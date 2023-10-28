ef655d7bc4f5bb5f7a53ff9ecef54447c7a422f6
chilejay7 <codyburk7@yahoo.com>
2023-10-28 12:31:12 -0600

    removed .super property used during testing

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -66,7 +66,7 @@ describe('Triangle', () => {
         it('should render the svg logo when the render method is called', () => {
             const triangle = new Triangle(text, color, background);
            
-            expect(triangle.super.render()).toMatch(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+            expect(triangle.render()).toMatch(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
             <polygon points="150, 18 244, 182 56, 182" fill="${background}" />
 
