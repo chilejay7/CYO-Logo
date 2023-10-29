d09ec8c4835061781b49077e72115d552b2c659f
chilejay7 <codyburk7@yahoo.com>
2023-10-29 14:38:02 -0600

    added render tests for all shapes

--- test/shapes.test.js
+++ test/shapes.test.js
@@ -64,7 +64,7 @@ describe('Triangle', () => {
             const triangle = new Triangle(text, color, background);
             console.log(triangle.render())
            
-            expect(triangle.render()).toMatch(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><polygon points="150, 18 244, 182 56, 182" fill="${this.background}" /><text x="150" y="150" font-size="60" text-anchor="middle" fill="${this.color}">${this.text}</text></svg>`)
+            expect(triangle.render()).toMatch(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><polygon points="150, 18 244, 182 56, 182" fill="${background}" /><text x="150" y="150" font-size="60" text-anchor="middle" fill="${color}">${text}</text></svg>`)
         })
     })
 });
@@ -86,8 +86,8 @@ describe('Circle', () => {
 
     describe('Circle render function', () => {
         it('should render the svg logo text returned from the Circle class render function', () => {
-            const circle = new Circle();
-            expect(circle.render()).toMatch(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><circle cx="150" cy="100" r="80" fill="${this.background}" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="${this.color}">${this.text}</text></svg>`)
+            const circle = new Circle(text, color, background);
+            expect(circle.render()).toMatch(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><circle cx="150" cy="100" r="80" fill="${background}" /><text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text></svg>`)
         })
     })
 });
@@ -99,4 +99,11 @@ describe('Square', () => {
             expect(square).toBeInstanceOf(Square);
         })
     });
+
+    describe('Square render function', () => {
+        it('should render the svg logo text returned from the sqaure class render function', () => {
+            const square = new Square(text, color, background);
+            expect(square.render()).toMatch(`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg"><rect x="25" y="25" rx="10" ry="10" width="150" height="150" fill="${background}" /><text x="100" y="115" font-size="60" text-anchor="middle" fill="${color}">${text}</text></svg>`)
+        })
+    })
 });
